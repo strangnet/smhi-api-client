27e77ec39351027af30611a9c7e0409c99809434
Patrick Strang <strangnet@gmail.com>
2018-08-07 16:49:11 +0200

    Hourly and avg monthly temps

--- smhi/temperature.go
+++ smhi/temperature.go
@@ -8,9 +8,13 @@ import (
 
 // Temperature parameter definitions
 const (
-	TemperatureParameterAverageDaily = 2
-	TemperatureParameterMinimumDaily = 19
-	TemperatureParameterMaximumDaily = 20
+	TemperatureParameterHourly            = 1
+	TemperatureParameterAverageDaily      = 2
+	TemperatureParameterMinimumDaily      = 19
+	TemperatureParameterMaximumDaily      = 20
+	TemperatureParameterAverageMonthly    = 22
+	TemperatureParameterMinimumTwiceDaily = 26 // Not implemented
+	TemperatureParameterMaximumTwiceDaily = 27 // Not implemented
 )
 
 // TemperatureService is a service for the temperature queries
@@ -35,7 +39,7 @@ type TemperatureDataValue struct {
 	Quality string `json:"quality,omitempty"`
 }
 
-func getTemperatureData(ctx context.Context, client *Client, parameter int, station uint16, period string) (*TemperatureData, *http.Response, error) {
+func getTemperatureData(ctx context.Context, client *Client, parameter int, station uint32, period string) (*TemperatureData, *http.Response, error) {
 	dataURL := fmt.Sprintf("api/version/latest/parameter/%d/station/%d/period/%s/data.json", parameter, station, period)
 	req, err := client.NewRequest("GET", dataURL)
 	if err != nil {
@@ -78,8 +82,18 @@ func getParameterData(ctx context.Context, client *Client, parameter int, includ
 	return p, resp, nil
 }
 
+// GetHourlyTemperatures retrieves hourly temperatures from a station
+func (s *TemperatureService) GetHourlyTemperatures(ctx context.Context, station uint32, period string) (*TemperatureData, *http.Response, error) {
+	return getTemperatureData(ctx, s.client, TemperatureParameterHourly, station, period)
+}
+
+// GetStationsWithHourlyTemperatures retrives all stations with hourly temperatures
+func (s *TemperatureService) GetStationsWithHourlyTemperatures(ctx context.Context, includeInactive bool) (*Parameter, *http.Response, error) {
+	return getParameterData(ctx, s.client, TemperatureParameterHourly, includeInactive)
+}
+
 // GetAverageDailyTemperatures retrieves the average daily temperatures from a station
-func (s *TemperatureService) GetAverageDailyTemperatures(ctx context.Context, station uint16, period string) (*TemperatureData, *http.Response, error) {
+func (s *TemperatureService) GetAverageDailyTemperatures(ctx context.Context, station uint32, period string) (*TemperatureData, *http.Response, error) {
 	return getTemperatureData(ctx, s.client, TemperatureParameterAverageDaily, station, period)
 }
 
@@ -88,8 +102,18 @@ func (s *TemperatureService) GetStationsWithAverageDailyTemperatures(ctx context
 	return getParameterData(ctx, s.client, TemperatureParameterAverageDaily, includeInactive)
 }
 
+// GetAverageMonthlyTemperatures retrieves the average monthly temperatures from a station
+func (s *TemperatureService) GetAverageMonthlyTemperatures(ctx context.Context, station uint32, period string) (*TemperatureData, *http.Response, error) {
+	return getTemperatureData(ctx, s.client, TemperatureParameterAverageMonthly, station, period)
+}
+
+// GetStationsWithAverageMonthlyTemperatures retrieves all stations with average daily temperatures
+func (s *TemperatureService) GetStationsWithAverageMonthlyTemperatures(ctx context.Context, includeInactive bool) (*Parameter, *http.Response, error) {
+	return getParameterData(ctx, s.client, TemperatureParameterAverageMonthly, includeInactive)
+}
+
 // GetMinimumDailyTemperatures retrieves the minimum daily temperatures from a station
-func (s *TemperatureService) GetMinimumDailyTemperatures(ctx context.Context, station uint16, period string) (*TemperatureData, *http.Response, error) {
+func (s *TemperatureService) GetMinimumDailyTemperatures(ctx context.Context, station uint32, period string) (*TemperatureData, *http.Response, error) {
 	return getTemperatureData(ctx, s.client, TemperatureParameterMinimumDaily, station, period)
 }
 
@@ -99,7 +123,7 @@ func (s *TemperatureService) GetStationsWithMinimumDailyTemperatures(ctx context
 }
 
 // GetMaximumDailyTemperatures retrieves the maximum daily temperatures from a station
-func (s *TemperatureService) GetMaximumDailyTemperatures(ctx context.Context, station uint16, period string) (*TemperatureData, *http.Response, error) {
+func (s *TemperatureService) GetMaximumDailyTemperatures(ctx context.Context, station uint32, period string) (*TemperatureData, *http.Response, error) {
 	return getTemperatureData(ctx, s.client, TemperatureParameterMinimumDaily, station, period)
 }
 
--- smhi/temperature_test.go
+++ smhi/temperature_test.go
@@ -100,10 +100,7 @@ func TestTemperatureService_GetAverageDailyTemperatures_returns404(t *testing.T)
 	client, mux, _, teardown := setup()
 	defer teardown()
 
-	mux.HandleFunc("/api/version/latest/parameter/2/station/12345/period/latest-day/data.json", func(w http.ResponseWriter, r *http.Request) {
-		testMethod(t, r, "GET")
-		fmt.Fprint(w, `{"value": [{"from": 1533254401000, "to": 1533340800000, "ref": "2018-08-03", "value": "21.8", "quality": "Y"}]}`)
-	})
+	mux.HandleFunc("/api/version/latest/parameter/2/station/12345/period/latest-day/data.json", func(w http.ResponseWriter, r *http.Request) {})
 
 	_, resp, err := client.Temperatures.GetAverageDailyTemperatures(context.Background(), 12346, PeriodLatestDay)
 	if err != nil {
@@ -244,10 +241,7 @@ func TestTemperatureService_GetMinimumDailyTemperatures_returns404(t *testing.T)
 	client, mux, _, teardown := setup()
 	defer teardown()
 
-	mux.HandleFunc("/api/version/latest/parameter/19/station/12345/period/latest-day/data.json", func(w http.ResponseWriter, r *http.Request) {
-		testMethod(t, r, "GET")
-		fmt.Fprint(w, `{"value": [{"from": 1533254401000, "to": 1533340800000, "ref": "2018-08-03", "value": "21.8", "quality": "Y"}]}`)
-	})
+	mux.HandleFunc("/api/version/latest/parameter/19/station/12345/period/latest-day/data.json", func(w http.ResponseWriter, r *http.Request) {})
 
 	_, resp, err := client.Temperatures.GetMinimumDailyTemperatures(context.Background(), 12346, PeriodLatestDay)
 	if err != nil {
@@ -388,15 +382,40 @@ func TestTemperatureService_GetMaximumTemperatures_returns404(t *testing.T) {
 	client, mux, _, teardown := setup()
 	defer teardown()
 
-	mux.HandleFunc("/api/version/latest/parameter/20/station/12345/period/latest-day/data.json", func(w http.ResponseWriter, r *http.Request) {
-		testMethod(t, r, "GET")
-		fmt.Fprint(w, `{"value": [{"from": 1533254401000, "to": 1533340800000, "ref": "2018-08-03", "value": "21.8", "quality": "Y"}]}`)
-	})
+	mux.HandleFunc("/api/version/latest/parameter/20/station/12345/period/latest-day/data.json", func(w http.ResponseWriter, r *http.Request) {})
 
 	_, resp, err := client.Temperatures.GetMaximumDailyTemperatures(context.Background(), 12346, PeriodLatestDay)
 	if err != nil {
 		if resp.StatusCode != http.StatusNotFound {
 			t.Errorf("Temperatures.GetMaximumDailyTemperatures returned error code %d, expected %d", resp.StatusCode, http.StatusNotFound)
+		}
+	}
+}
+
+func TestTemperatureService_GetAverageMonthlyTemperatures_returns404(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/api/version/latest/parameter/22/station/12345/period/latest-months/data.json", func(w http.ResponseWriter, r *http.Request) {})
+
+	_, resp, err := client.Temperatures.GetAverageMonthlyTemperatures(context.Background(), 12346, PeriodLatestDay)
+	if err != nil {
+		if resp.StatusCode != http.StatusNotFound {
+			t.Errorf("Temperatures.GetAverageMonthlyTemperatures returned error code %d, expected %d", resp.StatusCode, http.StatusNotFound)
+		}
+	}
+}
+
+func TestTemperatureService_GetHourlyTemperatures_returns404(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/api/version/latest/parameter/1/station/12345/period/latest-day/data.json", func(w http.ResponseWriter, r *http.Request) {})
+
+	_, resp, err := client.Temperatures.GetHourlyTemperatures(context.Background(), 12346, PeriodLatestDay)
+	if err != nil {
+		if resp.StatusCode != http.StatusNotFound {
+			t.Errorf("Temperatures.GetHourlyTemperatures returned error code %d, expected %d", resp.StatusCode, http.StatusNotFound)
 		}
 	}
 }
@@ -526,4 +545,258 @@ func TestTemperatureService_GetStationsWithMaximumDailyTemperatures_returnsOK(t 
 		}
 	})
 
+}
+
+func TestTemperatureService_GetStationsWithHourlyTemperatures_returnsOK(t *testing.T) {
+
+	data := `{
+		"key": "1",
+		"updated": 1533495600000,
+		"title": "Lufttemperatur: Välj station (sedan tidsutsnitt)",
+		"summary": "momentanvärde, 1 gång/tim",
+		"valueType": "SAMPLING",
+		"station": [
+		{
+			"name": "Abelvattnet Aut",
+			"owner": "SMHI",
+			"id": 154860,
+			"height": 665,
+			"latitude": 65.53,
+			"longitude": 14.97,
+			"active": true,
+			"key": "154860",
+			"updated": 841535999000,
+			"title": "Lufttemperatur - Abelvattnet Aut",
+			"summary": "Latitud: 65.5300 Longitud: 14.9700 Höjd: 665.0"
+		},
+		{
+			"name": "Abisko",
+			"owner": "SMHI",
+			"id": 188800,
+			"height": 388,
+			"latitude": 68.3557,
+			"longitude": 18.8206,
+			"active": false,
+			"key": "188800",
+			"updated": 1533081599000,
+			"title": "Lufttemperatur - Abisko",
+			"summary": "Latitud: 68.3557 Longitud: 18.8206 Höjd: 388.0"
+		}
+		]
+	}`
+
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/api/version/latest/parameter/1.json", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(w, data)
+	})
+
+	t.Run("includes all stations", func(t *testing.T) {
+		p, _, err := client.Temperatures.GetStationsWithHourlyTemperatures(context.Background(), true)
+		if err != nil {
+			t.Errorf("Temperatures.GetStationsWithHourlyTemperatures returned error: %v", err)
+		}
+
+		want := &Parameter{
+			Key:       "1",
+			Updated:   1533495600000,
+			Title:     "Lufttemperatur: Välj station (sedan tidsutsnitt)",
+			Summary:   "momentanvärde, 1 gång/tim",
+			ValueType: "SAMPLING",
+			Station: []Station{
+				{
+					Name:      "Abelvattnet Aut",
+					Owner:     "SMHI",
+					ID:        154860,
+					Height:    665,
+					Latitude:  65.53,
+					Longitude: 14.97,
+					Active:    true,
+					Key:       "154860",
+					Updated:   841535999000,
+					Title:     "Lufttemperatur - Abelvattnet Aut",
+					Summary:   "Latitud: 65.5300 Longitud: 14.9700 Höjd: 665.0",
+				},
+				{
+					Name:      "Abisko",
+					Owner:     "SMHI",
+					ID:        188800,
+					Height:    388,
+					Latitude:  68.3557,
+					Longitude: 18.8206,
+					Active:    false,
+					Key:       "188800",
+					Updated:   1533081599000,
+					Title:     "Lufttemperatur - Abisko",
+					Summary:   "Latitud: 68.3557 Longitud: 18.8206 Höjd: 388.0",
+				},
+			},
+		}
+
+		if !reflect.DeepEqual(p, want) {
+			t.Errorf("Temperatures.GetStationsWithHourlyTemperatures returned %+v, want %+v", p, want)
+		}
+	})
+	t.Run("includes only active stations", func(t *testing.T) {
+		p, _, err := client.Temperatures.GetStationsWithHourlyTemperatures(context.Background(), false)
+		if err != nil {
+			t.Errorf("Temperatures.GetStationsWithHourlyTemperatures returned error: %v", err)
+		}
+
+		want := &Parameter{
+			Key:       "1",
+			Updated:   1533495600000,
+			Title:     "Lufttemperatur: Välj station (sedan tidsutsnitt)",
+			Summary:   "momentanvärde, 1 gång/tim",
+			ValueType: "SAMPLING",
+			Station: []Station{
+				{
+					Name:      "Abelvattnet Aut",
+					Owner:     "SMHI",
+					ID:        154860,
+					Height:    665,
+					Latitude:  65.53,
+					Longitude: 14.97,
+					Active:    true,
+					Key:       "154860",
+					Updated:   841535999000,
+					Title:     "Lufttemperatur - Abelvattnet Aut",
+					Summary:   "Latitud: 65.5300 Longitud: 14.9700 Höjd: 665.0",
+				},
+			},
+		}
+
+		if !reflect.DeepEqual(p, want) {
+			t.Errorf("Temperatures.GetStationsWithHourlyTemperatures returned %+v, want %+v", p, want)
+		}
+	})
+}
+
+func TestTemperatureService_GetStationsWithAverageMonthlyTemperatures_returnsOK(t *testing.T) {
+
+	data := `{
+		"key": "22",
+		"updated": 1533495600000,
+		"title": "Lufttemperatur: Välj station (sedan tidsutsnitt)",
+		"summary": "medel, 1 gång per månad",
+		"valueType": "INTERVAL",
+		"station": [
+		{
+			"name": "Abelvattnet Aut",
+			"owner": "SMHI",
+			"id": 154860,
+			"height": 665,
+			"latitude": 65.53,
+			"longitude": 14.97,
+			"active": true,
+			"key": "154860",
+			"updated": 841535999000,
+			"title": "Lufttemperatur - Abelvattnet Aut",
+			"summary": "Latitud: 65.5300 Longitud: 14.9700 Höjd: 665.0"
+		},
+		{
+			"name": "Abisko",
+			"owner": "SMHI",
+			"id": 188800,
+			"height": 388,
+			"latitude": 68.3557,
+			"longitude": 18.8206,
+			"active": false,
+			"key": "188800",
+			"updated": 1533081599000,
+			"title": "Lufttemperatur - Abisko",
+			"summary": "Latitud: 68.3557 Longitud: 18.8206 Höjd: 388.0"
+		}
+		]
+	}`
+
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/api/version/latest/parameter/22.json", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+		fmt.Fprint(w, data)
+	})
+
+	t.Run("includes all stations", func(t *testing.T) {
+		p, _, err := client.Temperatures.GetStationsWithAverageMonthlyTemperatures(context.Background(), true)
+		if err != nil {
+			t.Errorf("Temperatures.GetStationsWithAverageMonthlyTemperatures returned error: %v", err)
+		}
+
+		want := &Parameter{
+			Key:       "22",
+			Updated:   1533495600000,
+			Title:     "Lufttemperatur: Välj station (sedan tidsutsnitt)",
+			Summary:   "medel, 1 gång per månad",
+			ValueType: "INTERVAL",
+			Station: []Station{
+				{
+					Name:      "Abelvattnet Aut",
+					Owner:     "SMHI",
+					ID:        154860,
+					Height:    665,
+					Latitude:  65.53,
+					Longitude: 14.97,
+					Active:    true,
+					Key:       "154860",
+					Updated:   841535999000,
+					Title:     "Lufttemperatur - Abelvattnet Aut",
+					Summary:   "Latitud: 65.5300 Longitud: 14.9700 Höjd: 665.0",
+				},
+				{
+					Name:      "Abisko",
+					Owner:     "SMHI",
+					ID:        188800,
+					Height:    388,
+					Latitude:  68.3557,
+					Longitude: 18.8206,
+					Active:    false,
+					Key:       "188800",
+					Updated:   1533081599000,
+					Title:     "Lufttemperatur - Abisko",
+					Summary:   "Latitud: 68.3557 Longitud: 18.8206 Höjd: 388.0",
+				},
+			},
+		}
+
+		if !reflect.DeepEqual(p, want) {
+			t.Errorf("Temperatures.GetStationsWithAverageMonthlyTemperatures returned %+v, want %+v", p, want)
+		}
+	})
+	t.Run("includes only active stations", func(t *testing.T) {
+		p, _, err := client.Temperatures.GetStationsWithAverageMonthlyTemperatures(context.Background(), false)
+		if err != nil {
+			t.Errorf("Temperatures.GetStationsWithAverageMonthlyTemperatures returned error: %v", err)
+		}
+
+		want := &Parameter{
+			Key:       "22",
+			Updated:   1533495600000,
+			Title:     "Lufttemperatur: Välj station (sedan tidsutsnitt)",
+			Summary:   "medel, 1 gång per månad",
+			ValueType: "INTERVAL",
+			Station: []Station{
+				{
+					Name:      "Abelvattnet Aut",
+					Owner:     "SMHI",
+					ID:        154860,
+					Height:    665,
+					Latitude:  65.53,
+					Longitude: 14.97,
+					Active:    true,
+					Key:       "154860",
+					Updated:   841535999000,
+					Title:     "Lufttemperatur - Abelvattnet Aut",
+					Summary:   "Latitud: 65.5300 Longitud: 14.9700 Höjd: 665.0",
+				},
+			},
+		}
+
+		if !reflect.DeepEqual(p, want) {
+			t.Errorf("Temperatures.GetStationsWithAverageMonthlyTemperatures returned %+v, want %+v", p, want)
+		}
+	})
 }
